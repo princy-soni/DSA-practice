e30d33c8f36d741faf4715435544a61e1fadcbdf
princy-soni <92619417+princy-soni@users.noreply.github.com>
2022-09-15 23:55:20 +0530

    ok

---
+++ .cph/hackerrank/hack10.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, i, lc = 0, uc = 0, sc = 0, d = 0, more = 0;
+    scanf("%d", &n);
+    char s[n];
+    scanf("%s", s);
+    for (i = 0; i < n; i++)
+    {
+        if (s[i] >= 'A' && s[i] <= 'Z')
+            uc = 1;
+        else if (s[i] >= 'a' && s[i] <= 'z')
+            lc = 1;
+        else if (s[i] >= '0' && s[i] <= '9')
+            d = 1;
+        else
+        {
+            sc = 1;
+        }
+    }
+    if (sc == 0)
+        more++;
+    if (lc == 0)
+        more++;
+    if (uc == 0)
+        more++;
+    if (d == 0)
+        more++;
+    if (n < 6)
+    {
+        if (6 - n > more)
+            printf("%d", 6 - n);
+        else
+            printf("%d", more);
+    }
+    else
+        printf("%d", more);
+    return 0;
+}
