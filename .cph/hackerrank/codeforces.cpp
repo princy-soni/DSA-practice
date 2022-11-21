db13347137886e3ba183d5aa69f6f7c65020ed34
princy-soni <92619417+princy-soni@users.noreply.github.com>
2022-11-21 21:34:16 +0530

    ok

---
+++ .cph/hackerrank/codeforces.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while (t--){
+        string s;
+        cin>>s;
+        int n;
+        n=s.size();string m="";
+        for (int i=1; i<=100;i++){
+            m=m+="Yes";
+        }
+        bool ok=false;
+        for (int i=0; i<m.size();i++){
+            if (i+n-1<m.size() && m.substr(i,n)==s){
+                ok=true;
+                break;
+            }
+        } 
+        if (ok){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+}
